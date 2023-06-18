7a1d1312f2c1f241bdd25c0f5878de8ca919df66
Likith54 <nlikith007@live.com>
2023-06-18 12:07:57 +0530

    Login page changes

--- src/app/Component/login/login.component.ts
+++ src/app/Component/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  form!: FormGroup;
+  public loginInvalid: boolean = false;
+  constructor(private fb:FormBuilder) { }
 
   ngOnInit(): void {
+    this.form = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
   }
+onSubmit(){
+  this.loginInvalid = false;
 
+}
+onCancel(){
+  this.form.reset();
+}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule,CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './Component/login/login.component';
 import { CoursesComponent } from './Component/courses/courses.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import {MatInputModule} from '@angular/material/input';
+import {MatCheckboxModule} from '@angular/material/checkbox'
 
 @NgModule({
   declarations: [
@@ -16,7 +18,10 @@ import { NoopAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NoopAnimationsModule,         
+    NoopAnimationsModule, 
+    MatInputModule,
+    MatCheckboxModule,
+    ReactiveFormsModule            
   ],
   providers: [],
   bootstrap: [AppComponent]
